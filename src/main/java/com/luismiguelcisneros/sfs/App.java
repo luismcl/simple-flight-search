c4d003abe6fbf3daeffb334f1b25657a3ca0bcf0
luismcl <yo@luismiguelcisneros.com>
2017-05-08 14:07:57 +0200

    Pre configure App

---
+++ src/main/java/com/luismiguelcisneros/sfs/App.java
@@ -0,0 +1,13 @@
+package com.luismiguelcisneros.sfs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
